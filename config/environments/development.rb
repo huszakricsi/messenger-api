eaa96d9fd358a78b6e1c298cb11e5fcaf75ea0b6
Huszák Richárd János <huszakricsi@gmail.com>
2018-10-15 12:04:28 +0200

    Described in development.rb how to use websockets outside localhost.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Rails.application.config.action_cable.allowed_request_origins = ['http://192.168.53.131:4200'] It allows the app to use websockets outside localhost
 end
